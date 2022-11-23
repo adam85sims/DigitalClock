18e8bff28abccb2abef7671daf0c6c0c31d04940
Adam <adam85sims@gmail.com>
2022-11-23 12:43:33 +0000

    Date and time pulled from local machine wrapped in JFrame

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,31 @@
+import java.util.Date;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Date;
+
 public class Main {
-    
-}
+    //get the current date and time on local machine and update every second
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Clock");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLayout(new FlowLayout());
+        frame.setVisible(true);
+        JLabel label = new JLabel();
+        frame.add(label);
+        while (true) {
+            Date date = new Date();
+            label.setText(date.toString());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    } //end of main
+
+} //end of class
